a2263bd87404579173a05ede476792f399dc6735
Henry Case <kris@amongbytes.com>
2019-07-23 12:06:09 +0100

    MSR

--- src/internal/utils/cycles.h
+++ src/internal/utils/cycles.h
@@ -28,11 +28,11 @@ static inline uint64_t cycles() {
   uint32_t pmuseren;
   uint32_t pmcntenset;
   // Read the user mode perf monitor counter access permissions.
-  asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
+  asm volatile("msr p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
   if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
-    asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
+    asm volatile("msr p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
     if (pmcntenset & 0x80000000ul) {  // Is it counting?
-      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
+      asm volatile("msr p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
       // The counter is set up to count every 64th cycle
       return (int64_t)(pmccntr) * 64;  // Should optimize to << 6
     }
